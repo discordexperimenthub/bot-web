dc845d9a84214fff20acd3a310652625cbdf0062
Voroniyx <julian.ruge@icloud.com>
2023-09-11 21:44:04 +0200

    got a first good idee for the discord components

--- src/components/discordComponents/automod.vue
+++ src/components/discordComponents/automod.vue
@@ -1,5 +1,5 @@
 <template>
-  <discord-messages>
+  <discord-messages class="test">
     <discord-message profile="dehBot">
       <discord-command
         slot="reply"
@@ -8,15 +8,43 @@
       ></discord-command>
       <discord-embed
         slot="embeds"
-        author-image="/sapphire.png"
-        author-name="Sapphire Developers"
-        author-url="https://sapphirejs.dev"
-        color="#0F52BA"
-        embed-title="Sapphire"
-        image="/sapphire.png"
-        thumbnail="/sapphire.png"
-        url="https://sapphirejs.dev"
+        author-image="/automod.png"
+        author-name="AutoMod"
+        color="rgba(84, 97, 242, 1)"
       >
+        <discord-embed-description slot="description">
+          <img src="/automod.png" height="22" width="22" class="inline-flex" />
+          <span class="font-bold ml-1 align-middle">{{ "automod title" }}</span
+          ><br />
+          <img src="/enabled.svg" height="16" width="16" class="inline-flex" />
+          <span class="align-middle"> Enabled</span><br />
+          <br />
+          <br />
+          <span class="font-bold align-middle">• Alert Channel:</span>
+          <span class="align-middle">None</span> <br />
+          <span class="font-bold align-middle">• Rules:</span>
+          <span class="align-middle">0 found</span><br />
+          <span class="font-bold align-middle">• AI Model:</span>
+          <span class="align-middle">
+            <code>gpt-3.5-turbo-16k</code> by OpenAI </span
+          ><br />
+          <span class="font-bold align-middle">• Allow Fallbacks: </span>
+          <img
+            src="/enabled.svg"
+            height="16"
+            width="16"
+            class="inline-flex"
+          /><span class="align-middle"> Enabled</span> <br />
+          <span class="font-bold align-middle"
+            >• Enabled Blacklisted Roles:</span
+          ><br />
+          <span class="font-bold align-middle">• Blacklisted Channels:</span
+          ><br />
+        </discord-embed-description>
+
+        <discord-embed-footer slot="footer" footer-image="/automod.png">
+          Made with ❤️ by discord.gg/experiments
+        </discord-embed-footer>
       </discord-embed>
     </discord-message>
   </discord-messages>
