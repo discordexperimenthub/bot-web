2834eddd1b685ae1a868aa223c426c352b3f8a3e
Voroniyx <julian.ruge@icloud.com>
2023-09-18 20:49:45 +0200

    some navbar changes

--- src/components/Site/NavBar.vue
+++ src/components/Site/NavBar.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="relative flex flex-wrap items-center justify-between px-2 py-3 bg-deh-black">
-        <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
-            <div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
+    <div class="relative flex flex-wrap items-center justify-between px-2 py-3">
+        <div class="container px-4 mx-auto flex flex-wrap items-center justify-between ">
+            <div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start ">
                 <div class="w-full m-1 flex justify-start">
-                    <div class="text-deh-white rounded blue text-4xl font-bold p-2">
-                        {{ $t('navfoo.title') }} <span class="text-deh-white bg-deh-main rounded p-[2px] text-xs align-top font-bold">BOT</span>
+                    <div class="rounded text-deh-main text-4xl font-bold p-2  bg-neutral-700 box-shadow-custom-black">
+                        {{ $t('navfoo.title') }} <span class="text-deh-white bg-deh-main rounded p-[2px] text-xs align-top font-bold lg:drop-shadow-[0_2px_20px_rgba(84,97,242,0.5)]">BOT</span>
                     </div>
                 </div>
                 <button
@@ -13,25 +13,31 @@
                     🟰
                 </button>
             </div>
+
+
+            <div v-if="showMenu">
+                
+            </div>
+
             <div v-bind:class="{ 'hidden': !showMenu, 'flex': showMenu }"
-                class="lg:flex lg:flex-grow items-center text-deh-main bg-deh-server-slider rounded lg:bg-transparent w-full lg:w-auto">
-                <ul class="flex flex-col lg:flex-row list-none lg:ml-auto navbar-ul-item">
-                    <li class="navbar-li-item">
-                        <a class="px-3 py-2 flex items-center text-xs uppercase font-bold hover:opacity-75">
-                            <span class="ml-2 lg:drop-shadow-[0_2px_20px_rgba(84,97,242,1)] text-xl">{{ $t('navfoo.invite') }}</span>
+                class="lg:flex lg:flex-grow items-center text-deh-main rounded lg:bg-transparent w-full lg:w-auto">
+                <div class="flex flex-col lg:flex-row list-none lg:ml-auto navbar-ul-item">
+                    <a class="navbar-li-item bg-neutral-700 box-shadow-custom-black mx-2 rounded" href="">
+                        <a class="p-[10px] flex items-center text-xs uppercase font-bold">
+                            <span class="mx-2 lg:drop-shadow-[0_2px_20px_rgba(84,97,242,0.50)] text-xl">{{ $t('navfoo.invite') }}</span>
                         </a>
-                    </li>
-                    <li class="navbar-li-item">
-                        <a class="px-3 py-2 flex items-center text-xs uppercase font-bold hover:opacity-75">
-                            <span class="ml-2 lg:drop-shadow-[0_2px_20px_rgba(84,97,242,1)] text-xl">{{ $t('navfoo.support') }}</span>
+                    </a>
+                    <a class="navbar-li-item bg-neutral-700 box-shadow-custom-black mx-2 rounded" href="">
+                        <a class="p-[10px] flex items-center text-xs uppercase font-bold">
+                            <span class="mx-2 lg:drop-shadow-[0_2px_20px_rgba(84,97,242,0.50)] text-xl">{{ $t('navfoo.support') }}</span>
                         </a>
-                    </li>
+                    </a>
                     <!-- <li class="navbar-li-item">
                         <a class="px-3 py-2 flex items-center text-xs uppercase font-bold hover:opacity-75">
                             <span class="ml-2 lg:drop-shadow-[0_2px_20px_rgba(84,97,242,1)] text-xl">{{ $t('navfoo.docs') }}</span>
                         </a>
                     </li> -->
-                </ul>
+                </div>
             </div>
         </div>
     </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,47 +1,51 @@
 <template>
+  <div class="bg-deh-black">
     <NavBar />
-    <main class="bg-deh-black min-h-screen pt-28">
-        <section class="w-full mb-28 flex flex-col items-center">
-            <!-- Landing -->
+    <main class="min-h-screen pt-28">
+      <section class="w-full mb-28 flex flex-col items-center">
+        <!-- Landing -->
 
-            <img src="/logo.png" class="lg:w-[15%] m-2 p-2">
+        <img src="/logo.png" class="lg:w-[15%] m-2 p-2" />
 
-            <div class="p-2 m-1 text-center font-bold text-white">
-                {{ $t('home.description') }}
-                <br>
-                <a href="https://discord.gg/experiments" class="text-deh-main">https://discord.gg/experiments</a>
-            </div>
+        <div class="p-2 m-1 text-center font-bold text-white">
+          {{ $t("home.description") }}
+          <br />
+          <a href="https://discord.gg/experiments" class="text-deh-main"
+            >https://discord.gg/experiments</a
+          >
+        </div>
 
-            <div class="my-4">
-                <!-- Buttons -->
-                <a href="https://discord.com/api/oauth2/authorize?client_id=1078340529932222505&permissions=536882192&scope=applications.commands%20bot"
-                    class="bg-deh-main p-1 rounded text-deh-white font-bold text-3xl mx-2">
-                    <button>
-                        {{ $t('navfoo.invite') }}
-                    </button>
-                </a>
-            </div>
+        <div class="my-4">
+          <!-- Buttons -->
+          <a
+            href="https://discord.com/api/oauth2/authorize?client_id=1078340529932222505&permissions=536882192&scope=applications.commands%20bot"
+            class="bg-deh-main p-1 rounded text-deh-white font-bold text-3xl mx-2"
+          >
+            <button>
+              {{ $t("navfoo.invite") }}
+            </button>
+          </a>
+        </div>
+      </section>
 
-        </section>
-        
-        <Servers />
+      <Servers />
 
-        <section class="mt-4">
-            <div class="font-bold text-deh-white text-center text-5xl underline">
-                {{ $t('feature.title') }}
-            </div>
-            <Features />
-        </section>
+      <section class="mt-4">
+        <div class="font-bold text-deh-white text-center text-5xl underline">
+          {{ $t("feature.title") }}
+        </div>
+        <Features />
+      </section>
 
-        <img src="../assets/img/footer.top.svg" class="fill-deh-server-slider">
-        <Footer />
+      <img src="../assets/img/footer.top.svg" class="fill-deh-server-slider" />
+      <Footer />
     </main>
+  </div>
 </template>
 
 <script setup lang="ts">
-import NavBar from '../components/Site/NavBar.vue';
-import Footer from '../components/Site/Footer.vue';
-import Features from '../components/Site/Features.vue';
-import Servers from '../components/Site/Servers.vue';
-
+import NavBar from "../components/Site/NavBar.vue";
+import Footer from "../components/Site/Footer.vue";
+import Features from "../components/Site/Features.vue";
+import Servers from "../components/Site/Servers.vue";
 </script>
