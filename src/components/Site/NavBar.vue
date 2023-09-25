ef621f5ee874a5c090838ff48c255a6b5015b380
Voroniyx <julian.ruge@icloud.com>
2023-09-25 20:22:42 +0200

    Mobile Bot Icon smaller

--- src/components/Site/NavBar.vue
+++ src/components/Site/NavBar.vue
@@ -11,18 +11,6 @@
               ↗️
             </button>
 
-            <!-- 
-              TODO: Mobile NavBar Icons [✓]
-              TODO: Desktop NavBar Link List Animation [✓]
-              TODO: Desktop NavBar Elements more to the right [✓] 
-              TODO: NavBar Global listen to resize event [✓]
-              TODO: Mobile Bot Icon smaller [ ]
-              TODO: Desktop Feature margin remove [ ]
-              TODO: Desktop Recreate the Title Text [ ]
-              TODO: Desktop Recreate Landing Screen [ ]
-              TODO: Mobile NavBar Popout justify center [ ]
-             -->
-
             <div
               class="rounded text-deh-main text-4xl max-w-fit font-bold p-2 bg-neutral-700 box-shadow-custom-black mt-4">
               {{ $t("navfoo.title") }}
